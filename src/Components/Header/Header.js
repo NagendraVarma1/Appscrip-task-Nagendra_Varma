7272c4089ec8391dbd13bb84ee351418e9d25e27
NagendraVarma1 <nagendravarmal512@gmail.com>
2024-10-09 10:05:19 +0530

    Styling for some components for mobile view

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,23 +5,35 @@ import heart from "../../Assests/heart.svg";
 import bag from "../../Assests/shopping-bag.svg";
 import profile from "../../Assests/profile.svg";
 import language from "../../Assests/Language.svg";
-import hamburger from '../../Assests/solar_hamburger-menu-linear.svg'
-import brandAndLogo from '../../Assests/Frame 32.svg'
+import hamburger from "../../Assests/solar_hamburger-menu-linear.svg";
+import brandAndLogo from "../../Assests/Frame 32.svg";
 
 const Header = () => {
   return (
     <div className={classes.mainHeader}>
       <div className={classes.headerStrip}>
-      <img className={classes.brandAndLogo1} src={brandAndLogo} alt="brand and logo"/>
-        <img className={classes.brandAndLogo2} src={brandAndLogo} alt="brand and logo"/>
-        <img className={classes.brandAndLogo3} src={brandAndLogo} alt="brand and logo"/>
+        <img
+          className={classes.brandAndLogo1}
+          src={brandAndLogo}
+          alt="brand and logo"
+        />
+        <img
+          className={classes.brandAndLogo2}
+          src={brandAndLogo}
+          alt="brand and logo"
+        />
+        <img
+          className={classes.brandAndLogo3}
+          src={brandAndLogo}
+          alt="brand and logo"
+        />
       </div>
       <div className={classes.head}>
         <div className={classes.header}>
-        <img className={classes.hamburger} src={hamburger} alt="hamburger" />
+          <img className={classes.hamburger} src={hamburger} alt="hamburger" />
           <img className={classes.logo} src={logo} alt="logo" />
           <p className={classes.brand}>LOGO</p>
-          <div className={classes.icons}>
+          <div className={classes.icons}> 
             <img className={classes.icon1} src={search} alt="icon" />
             <img className={classes.icon2} src={heart} alt="icon" />
             <img className={classes.icon3} src={bag} alt="icon" />
@@ -30,11 +42,11 @@ const Header = () => {
           </div>
         </div>
         <ul className={classes.mainTab}>
-          <li className={classes.tab}>SHOP</li>
-          <li className={classes.tab}>SKILLS</li>
-          <li className={classes.tab}>STORIES</li>
-          <li className={classes.tab}>ABOUT</li>
-          <li className={classes.tab}>CONTACT US</li>
+          <li className={classes.tab1}>SHOP</li>
+          <li className={classes.tab2}>SKILLS</li>
+          <li className={classes.tab3}>STORIES</li>
+          <li className={classes.tab4}>ABOUT</li>
+          <li className={classes.tab5}>CONTACT US</li>
         </ul>
       </div>
     </div>
